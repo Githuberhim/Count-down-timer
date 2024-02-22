3a963b2c91550a3c5f3152e9ae9dae9ca11f16c9
Githuberhim <145221614+Githuberhim@users.noreply.github.com>
2024-02-23 00:38:28 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,23 @@
+const endDate="1 jan 2025 12:00 AM";
+document.getElementById("end-date").innerText=endDate;
+const input=document.querySelectorAll("input");
+function clock(){
+    const end=new Date(endDate);
+    const now=new Date();
+    const diff=(end-now)/1000;
+    if(diff<=0){
+        document.getElementById("exp").innerText="Expired!!";
+        return;
+    }
+    input[0].value=Math.floor(diff/3600/24);
+    input[1].value=Math.floor((diff/3600)%24);
+    input[2].value=Math.floor((diff/60)%60);
+    input[3].value=Math.floor(diff%60);
+    
+}
+clock();
+setInterval(
+    ()=>{
+        clock()
+    },1000
+)
